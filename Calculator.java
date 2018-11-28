e4ecf5d2aea153c0fce5ef34b37d53746463fca0
memoromoj <memo.romo.2095@gmail.com>
2018-11-28 12:59:55 -0600

    resta??

--- Calculator.java
+++ Calculator.java
@@ -1,9 +1,12 @@
 class Calculator {
     public static void main(String[] args) {
         System.out.println(suma(1,2));
-
+        System.out.println(resta(9,2));
     }
     public int Suma(int a, int b){
       return a+b;
+    }
+    public int Resta(int a, int b){
+      return a-b;
     }
 }
